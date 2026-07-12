3f1789ca30bad6fcae48d8156efa39c1e46ff998
abhishekkhot <abhishek.khot@idfy.com>
2026-07-12 12:00:49 +0530

    init commit

---
+++ src/main/java/com/batchforge/BatchforgeApplication.java
@@ -0,0 +1,13 @@
+package com.batchforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batchforge/BatchforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batchforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
